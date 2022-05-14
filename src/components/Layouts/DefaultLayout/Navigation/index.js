e9c906321133dc38e9a140417c23c90f85e182b1
river248 <85404917+river248@users.noreply.github.com>
2022-05-15 03:38:13 +0700

    Feature: Current Page at Navigation.

--- src/components/Layouts/DefaultLayout/Navigation/index.js
+++ src/components/Layouts/DefaultLayout/Navigation/index.js
@@ -5,45 +5,48 @@ import Tippy from '@tippyjs/react/headless'
 import styles from './Navigation.module.scss'
 import Button from '~/components/Button'
 import { Wrapper as PopperWrapper } from '~/components/Popper'
+import { useLocation } from 'react-router-dom'
 
 const cx = classNames.bind(styles)
 
+const navItems = [
+    { name: 'TRANG CHỦ', path: '/' },
+    {
+        name: 'ÁO',
+        categories: [
+            { id: 'somi', name: 'Sơ mi', path: '/product/top?type=somi' },
+            { id: 'aokieu', name: 'Áo kiểu', path: '/product/top?type=aokieu' },
+            { id: 'thun', name: 'Áo thun', path: '/product/top?type=aothun' },
+        ],
+    },
+    {
+        name: 'QUẦN',
+        categories: [
+            { id: 'somi', name: 'Quần jean', path: '/product/bottom?type=jean' },
+            { id: 'quankieu', name: 'Quần kiểu', path: '/product/bottom?type=quankieu' },
+        ],
+    },
+    {
+        name: 'VÁY',
+        categories: [
+            { id: 'chanvay', name: 'Chân váy', path: '/product/bottom-girl?type=chanvay' },
+            { id: 'dam', name: 'Đầm', path: '/product/bottom-girl?type=dam' },
+        ],
+    },
+    {
+        name: 'TIPS',
+        categories: [
+            { id: 'tip1', name: 'Đi học mặc gì?', path: '/tip/1' },
+            { id: 'tip2', name: 'Công sở mặc gì?', path: '/tip/2' },
+            { id: 'tip3', name: 'Tips phối màu đỉnh', path: '/tip/3' },
+        ],
+    },
+    { name: 'GIỚI THIỆU', path: '/introduce' },
+    { name: 'LIÊN HỆ', path: '/contact' },
+]
+
 function Navigation() {
-    const navItems = [
-        { name: 'TRANG CHỦ', path: '/' },
-        {
-            name: 'ÁO',
-            categories: [
-                { id: 'somi', name: 'Sơ mi', path: '/top?type=somi' },
-                { id: 'aokieu', name: 'Áo kiểu', path: '/top?type=aokieu' },
-                { id: 'thun', name: 'Áo thun', path: '/top?type=aothun' },
-            ],
-        },
-        {
-            name: 'QUẦN',
-            categories: [
-                { id: 'somi', name: 'Quần jean', pahth: '/bottom?type=jean' },
-                { id: 'quankieu', name: 'Quần kiểu', pahth: '/bottom?type=quankieu' },
-            ],
-        },
-        {
-            name: 'VÁY',
-            categories: [
-                { id: 'chanvay', name: 'Chân váy', path: '/bottom-girl?type=chanvay' },
-                { id: 'dam', name: 'Đầm', path: '/bottom-girl?type=dam' },
-            ],
-        },
-        {
-            name: 'TIPS',
-            categories: [
-                { id: 'tip1', name: 'Đi học mặc gì?', path: '/tip/1' },
-                { id: 'tip2', name: 'Công sở mặc gì?', path: '/tip/2' },
-                { id: 'tip3', name: 'Tips phối màu đỉnh', path: '/tip/3' },
-            ],
-        },
-        { name: 'GIỚI THIỆU', path: '/introduce' },
-        { name: 'LIÊN HỆ', path: '/contact' },
-    ]
+    const location = useLocation()
 
     return (
         <nav className={cx('wrapper')}>
@@ -55,6 +58,7 @@ function Navigation() {
                                 <Button className={cx('custom-btn')} to={navItem.path}>
                                     {navItem.name}
                                 </Button>
+                                {navItem.path === location.pathname && <div className={cx('current-page')} />}
                             </li>
                         )
                     }
@@ -87,6 +91,9 @@ function Navigation() {
                                 <Button className={cx('custom-btn')} text>
                                     {navItem.name}
                                 </Button>
+                                {navItem.categories[0].path.includes(location.pathname.concat('?')) && (
+                                    <div className={cx('current-page')} />
+                                )}
                             </li>
                         </Tippy>
                     )
