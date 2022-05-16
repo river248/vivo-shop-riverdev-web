a2fdf8c752f38425f8375953e05c0bc03226250c
Phan Xuan Giang <phag@elcaNet.local>
2022-05-16 09:15:31 +0700

    Move imageURL from Card to Image.

--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -1,34 +1,16 @@
-import React, { useEffect, useState } from 'react'
+import React from 'react'
 import classNames from 'classnames/bind'
 import Tippy from '@tippyjs/react'
-import { getDownloadURL, ref } from 'firebase/storage'
 
 import styles from './Card.module.scss'
 import Image from '~/components/Image'
-import { storage } from '~/config/firebaseConfig'
 
 const cx = classNames.bind(styles)
 
 function Card({ image, ratio = 'ratio3x4', title, textStyle = '', className, children, onClick }) {
-    const [imageURL, setImageURL] = useState('')
     const _props = {
         onClick,
     }
-
-    useEffect(() => {
-        let isSubcribe = true
-
-        if (image && !image?.includes('/static'))
-            getDownloadURL(ref(storage, `${image}`))
-                .then((url) => {
-                    if (isSubcribe) setImageURL(url)
-                })
-                .catch((error) => console.log(error))
-        return () => {
-            setImageURL('')
-            isSubcribe = false
-        }
-    }, [image])
 
     const classes = cx('wrapper', {
         [className]: className,
@@ -36,15 +18,7 @@ function Card({ image, ratio = 'ratio3x4', title, textStyle = '', className, chi
     })
     return (
         <div className={classes} {..._props}>
-            {image && (
-                <>
-                    {!image.includes('/static') ? (
-                        <Image ratio={ratio} src={imageURL} alt={'candytop'} />
-                    ) : (
-                        <Image ratio={ratio} src={image} alt={'candytop'} />
-                    )}
-                </>
-            )}
+            {image && <Image ratio={ratio} src={image} alt={title} />}
             <Tippy placement="bottom-start" content={<span className={cx('tool-tip')}>{title}</span>}>
                 <div className={cx('title')}>{title}</div>
             </Tippy>
