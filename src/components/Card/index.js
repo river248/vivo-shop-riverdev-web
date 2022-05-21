c669f97278e7431317a82b23d69213a8331b3a0f
river248 <85404917+river248@users.noreply.github.com>
2022-05-21 13:57:35 +0700

    Feature: Detailed Product UI.

--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -1,15 +1,23 @@
 import React from 'react'
 import classNames from 'classnames/bind'
 import Tippy from '@tippyjs/react'
+import { Link } from 'react-router-dom'
 
 import styles from './Card.module.scss'
 import Image from '~/components/Image'
 
 const cx = classNames.bind(styles)
 
-function Card({ image, ratio = 'ratio3x4', title, textStyle = '', textAlign = '', className, children, onClick }) {
+function Card({ image, ratio = 'ratio3x4', title, textStyle = '', textAlign = '', className, children, to, onClick }) {
+    let Comp = 'div'
+
     const _props = {
         onClick,
+    }
+
+    if (to) {
+        _props.to = to
+        Comp = Link
     }
 
     const classes = cx('wrapper', {
@@ -18,10 +26,12 @@ function Card({ image, ratio = 'ratio3x4', title, textStyle = '', textAlign = ''
         [textAlign]: textAlign,
     })
     return (
-        <div className={classes} {..._props}>
+        <div className={classes}>
             {image && <Image ratio={ratio} src={image} alt={title} />}
             <Tippy placement="bottom" content={<span className={cx('tool-tip')}>{title}</span>}>
-                <div className={cx('title')}>{title}</div>
+                <Comp {..._props} className={cx('title')}>
+                    {title}
+                </Comp>
             </Tippy>
             <div className={cx('content')}>{children}</div>
         </div>
