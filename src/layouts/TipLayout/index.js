10ed29c33c7e681550589cfb1e5812641f197848
Phan Xuan Giang <giang.phan@elca.vn>
2022-06-09 10:50:34 +0700

    Feature: Build Tip Layout #2.

--- src/layouts/TipLayout/index.js
+++ src/layouts/TipLayout/index.js
@@ -3,10 +3,9 @@ import PropTypes from 'prop-types'
 import classNames from 'classnames/bind'
 
 import styles from './TipLayout.module.scss'
-import images from '~/assets/images'
-import HeaderImage from '~/components/HeaderImage'
-import ToTop from '~/components/ToTop'
+import Header from './Header'
 import SideContent from './SideContent'
+import ToTop from '~/components/ToTop'
 import { FlexWrapper, Wrapper } from '~/components/Popper'
 
 const cx = classNames.bind(styles)
@@ -14,12 +13,12 @@ const cx = classNames.bind(styles)
 function TipLayout({ children }) {
     return (
         <div className={cx('wrapper')}>
-            <HeaderImage className={cx('header-image')} image={images.tipBanner} alt={'Tip Banner'} />
+            <Header />
             <Wrapper flexWrapper>
                 <FlexWrapper className={cx('content')} xxl={9} xl={9} lg={9} md={8} sm={6}>
                     {children}
                 </FlexWrapper>
-                <FlexWrapper xxl={3} xl={3} lg={3} md={4} sm={6}>
+                <FlexWrapper className={cx('side-content')} xxl={3} xl={3} lg={3} md={4} sm={6}>
                     <SideContent />
                 </FlexWrapper>
             </Wrapper>
